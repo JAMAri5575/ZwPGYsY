4adc64f7d01b7a49dea88bb7d4fb2ae8cb456464
JAMAri5575 <losses.gainers0t@icloud.com>
2025-03-30 03:38:37 +0200

    Optimize the file's rendering speed by reducing DOM manipulation

--- PINoyn/Odvzb3/distributed-systems_ClWyj_1742586021.md.js
+++ PINoyn/Odvzb3/distributed-systems_ClWyj_1742586021.md.js
@@ -1,7 +1,6 @@
 module.exports = {
     contents: ['summary.md'],
     pathToPublic: 'pdf/advanced-java.pdf',
-    pdfOptions: '<options for puppeteer.pdf()>',
     removeTemp: true,
     emulateMedia: 'screen',
 };
